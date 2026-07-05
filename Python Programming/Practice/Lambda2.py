87a257a5e4e790ef757b19f88692c4bbb5fd8ea9
ruchakhabale <rucharkhabale@gmail.com>
2026-07-06 00:16:50 +0530

    Add files via upload

---
+++ Python Programming/Practice/Lambda2.py
@@ -0,0 +1,11 @@
+Cube = lambda No : No ** 3
+
+def main():
+    Value = int(input("Enter your number : "))
+
+    Ret = Cube(Value)
+
+    print("Square of the number is : ",Ret)
+
+if __name__ == "__main__":
+    main()
