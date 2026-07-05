5dea2256ca417800c98303c5ff2566bf4ceb7cc7
ruchakhabale <rucharkhabale@gmail.com>
2026-07-06 00:14:04 +0530

    Add files via upload

---
+++ Python Programming/Practice/Lambda1.py
@@ -0,0 +1,11 @@
+Sqaure = lambda No : No * No
+
+def main():
+    Value = int(input("Enter your number : "))
+
+    Ret = Sqaure(Value)
+
+    print("Square of the number is : ",Ret)
+
+if __name__ == "__main__":
+    main()
