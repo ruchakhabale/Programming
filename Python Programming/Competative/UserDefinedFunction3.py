e7c161d922df6403e147e438009a2a50f178bd6a
ruchakhabale <rucharkhabale@gmail.com>
2026-07-20 23:17:21 +0530

    Add files via upload

---
+++ Python Programming/Competative/UserDefinedFunction3.py
@@ -0,0 +1,15 @@
+# Accept : one parameter
+# Return : one value 
+
+def Marvellous(Value):
+    print("Inside Marvellous : ",Value)
+    return 21
+
+
+def main():
+    Ret = Marvellous(11)
+    print("Return Value is : ",Ret)
+
+if __name__ == "__main__":  
+    main() 
+    
