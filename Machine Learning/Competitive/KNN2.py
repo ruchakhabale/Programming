12c77a10ef02b92b119bbf69a0c4ff5f861a8815
ruchakhabale <rucharkhabale@gmail.com>
2026-08-14 23:35:17 +0530

    Add files via upload

---
+++ Machine Learning/Competitive/KNN2.py
@@ -0,0 +1,80 @@
+import math
+import numpy as np
+
+def EucDistance(P1, P2):
+    Ans = math.sqrt((P1['X'] - P2['X']) **2 + (P1['Y'] - P2['Y']) **2)
+    return Ans
+
+def KNNClassifier(k = 1):
+    border = "-"*50
+
+    Data = [
+        {'point' : 'A', 'X' : 1, "Y" : 2, 'label' : 'Red'},
+        {'point' : 'B', 'X' : 2, "Y" : 3, 'label' : 'Red'},
+        {'point' : 'C', 'X' : 3, "Y" : 1, 'label' : 'Blue'},
+        {'point' : 'D', 'X' : 4, "Y" : 5, 'label' : 'Blue'}
+    ]
+
+    print(border)
+    print("KNN Classifier")
+    print(border)
+
+    for i in Data:
+        print(i)
+
+    print(border)
+
+    new_point = {'X' : 3, 'Y' : 3}
+
+    print("Distances of all points : ")
+    print(border)
+
+    for d in Data:
+        d['distance'] = EucDistance(d,new_point)
+
+    for d in Data:
+        print(d)
+
+    sorted_data = sorted(Data, key= lambda item : item['distance'])
+
+    print(border)
+
+    nearest = sorted_data[:k]
+
+    print(border)
+    print("Nearest 1 member is : ")
+    print(border)
+
+    for d in nearest:
+        print(d)
+
+    votes = {}
+
+    for neighbours in nearest:
+        label = neighbours['label']
+        votes[label] = votes.get(label,0) + 1
+
+    print(border)
+    print("Voting result is : ")
+    print(border)
+
+    for d in votes:
+        print("Name : ",d, "Number of votes : ",votes[d])
+
+    print(border)
+
+    iMax = 0
+    Name = ""
+
+    for d in votes:
+        if(votes[d] > iMax):
+            iMax = votes[d]
+            Name = d
+
+    print("Final prediction is : ",Name)
+
+def main():
+    KNNClassifier()
+
+if __name__ == "__main__":
+    main()
