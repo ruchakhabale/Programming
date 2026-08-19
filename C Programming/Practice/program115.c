044a499e71b5319570d8c92e012cb43c697777db
ruchakhabale <rucharkhabale@gmail.com>
2026-08-19 21:40:05 +0530

    Add files via upload

---
+++ C Programming/Practice/program115.c
@@ -0,0 +1,26 @@
+//this has errors on purpose
+#include <stdio.h>
+
+void Display(int Arr[], int iSize)       
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+     
+}
+
+int main()
+{
+    int iLength = 4;
+
+    int Brr[iLength] = {10, 20, 30, 40};   //Error    
+
+    
+    Display(Brr, iLength);     
+
+    
+    return 0;
+}
