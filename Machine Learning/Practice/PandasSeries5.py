f6edafb42d4e2bdb12802a9d66cbdbc3e54b1042
ruchakhabale <rucharkhabale@gmail.com>
2026-08-26 23:28:15 +0530

    Add files via upload

---
+++ Machine Learning/Practice/PandasSeries5.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def main():
+    # Custom indexing 
+    sobj = pd.Series([11,21,51,101],index = [5,6,7,8])
+
+    print(sobj)
+
+    print(sobj[7])
+
+if __name__ == "__main__":
+    main()
---
+++ Machine Learning/Practice/PandasSeries6.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def main():
+    # Custom indexing
+    sobj = pd.Series([11,21,51,101],index = ["C","C++","Java","Python"])
+
+    print(sobj)
+
+    print(sobj["Python"])
+
+if __name__ == "__main__":
+    main()
