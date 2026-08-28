ade06316994cc8b045ea654bed472f5b38beaf6b
ruchakhabale <rucharkhabale@gmail.com>
2026-08-28 19:55:20 +0530

    Add files via upload

---
+++ Machine Learning/Practice/UserDefinedKNN5.py
@@ -0,0 +1,49 @@
+# chck again op 
+import math
+import numpy as np
+
+def MarvellousEucDistance(P1, P2):
+    Ans = math.sqrt((P1['X'] - P2['X']) **2 + (P1['Y'] - P2['Y']) ** 2)
+    return Ans
+
+
+def MarvellousKNNClassifier():
+    border = "-"*40
+
+    # List of dictionaries 
+    Data = [
+        {'point' : 'A', 'X' : 1, 'Y' : 2, 'label' : 'Red'},        # Data[0]
+        {'point' : 'B', 'X' : 2, 'Y' : 3, 'label' : 'Red'},        # Data[1]
+        {'point' : 'C', 'X' : 3, 'Y' : 1, 'label' : 'Blue'},       # Data[2]
+        {'point' : 'D', 'X' : 5, 'Y' : 6, 'label' : 'Blue'}        # Data[3]
+    ]
+
+    print(border)
+    print("Marvellous KNN Classifier")
+    print(border)
+
+    for i in Data:
+        print(i)
+
+    print(border)
+
+    new_point = {'X' : 3, 'Y' : 3}
+
+    print("Distances of all points : ")
+    print(border)
+
+
+    for d in Data:
+       d['distance'] = (MarvellousEucDistance(d,new_point))
+
+    for d in Data:
+        print(d['distance'], d['label'])
+
+    print(border)
+
+
+def main():
+    MarvellousKNNClassifier()
+
+if __name__ == "__main__":
+    main()
---
+++ Machine Learning/Practice/UserDefinedKNN9.py
@@ -0,0 +1,94 @@
+import math
+import numpy as np
+
+def MarvellousEucDistance(P1, P2):
+    Ans = math.sqrt((P1['X'] - P2['X']) **2 + (P1['Y'] - P2['Y']) ** 2)
+    return Ans
+
+
+def MarvellousKNNClassifier():
+    border = "-"*40
+
+    # List of dictionaries 
+    Data = [
+        {'point' : 'A', 'X' : 1, 'Y' : 2, 'label' : 'Red'},        # Data[0]
+        {'point' : 'B', 'X' : 2, 'Y' : 3, 'label' : 'Red'},        # Data[1]
+        {'point' : 'C', 'X' : 3, 'Y' : 1, 'label' : 'Blue'},       # Data[2]
+        {'point' : 'D', 'X' : 5, 'Y' : 6, 'label' : 'Blue'}        # Data[3]
+    ]
+
+    print(border)
+    print("Marvellous KNN Classifier")
+    print(border)
+
+    for i in Data:
+        print(i)
+
+    print(border)
+
+    new_point = {'X' : 3, 'Y' : 3}
+
+    print("Distances of all points : ")
+    print(border)
+
+    for d in Data: 
+        d['distance'] = MarvellousEucDistance(d,new_point)
+
+    for d in Data:
+        print(d)
+
+    sorted_data = sorted(Data, key= lambda item : item['distance'])
+
+    print(border)
+    print("Sorted data : ")
+    print(border)
+
+    for d in sorted_data:
+        print(d)
+
+    print(border)
+
+    k = 3
+
+    nearest = sorted_data[:k]     # slicing 
+
+    print(border)
+    print("Nearest 3 members are : ")
+    print(border)
+
+    for d in nearest:
+        print(d)
+
+    print(border)
+
+    # Voting 
+    votes = {}
+
+    for neighbours in nearest:
+        label = neighbours['label']
+        votes[label] = votes.get(label,0) + 1
+
+    print(border)
+    print("Voting result is: ")
+    print(border)
+
+    for d in votes:
+        print("Name : ",d,"Number of votes: ",votes[d])
+
+    print(border)
+
+    iMax = 0
+    Name = ""
+
+    for d in votes:
+        if(votes[d] > iMax):
+            iMax = votes[d] 
+            Name = d
+
+    print("Final prediction is: ",Name)
+
+def main():
+    MarvellousKNNClassifier()
+
+if __name__ == "__main__":
+    main()
