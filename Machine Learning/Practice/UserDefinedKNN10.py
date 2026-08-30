935620bc321bbc4781c16f0b947f7ed2fe0b6044
ruchakhabale <rucharkhabale@gmail.com>
2026-08-31 00:03:35 +0530

    Update UserDefinedKNN10.py

--- Machine Learning/Practice/UserDefinedKNN10.py
+++ Machine Learning/Practice/UserDefinedKNN10.py
@@ -1,27 +1,27 @@
 import math
 import numpy as np
 
-def MarvellousEucDistance(P1, P2):
+def EucDistance(P1, P2):
     Ans = math.sqrt((P1['X'] - P2['X']) **2 + (P1['Y'] - P2['Y']) ** 2)
     return Ans
 
 
-def MarvellousKNNClassifier(k = 3):
+def KNNClassifier(k = 3):
     border = "-"*40
 
     # List of dictionaries 
     Data = [
-        {'point' : 'A', 'X' : 1, 'Y' : 2, 'label' : 'Red'},        # Data[0]
-        {'point' : 'B', 'X' : 2, 'Y' : 3, 'label' : 'Red'},        # Data[1]
-        {'point' : 'C', 'X' : 3, 'Y' : 1, 'label' : 'Blue'},       # Data[2]
+        {'point' : 'A', 'X' : 1, 'Y' : 2, 'label' : 'Red'},        
+        {'point' : 'B', 'X' : 2, 'Y' : 3, 'label' : 'Red'},        
+        {'point' : 'C', 'X' : 3, 'Y' : 1, 'label' : 'Blue'},       
         {'point' : 'D', 'X' : 5, 'Y' : 6, 'label' : 'Blue'},
-        {'point' : 'E', 'X' : 6, 'Y' : 6, 'label' : 'Blue'},        # Data[0]
-        {'point' : 'F', 'X' : 3, 'Y' : 4, 'label' : 'Red'},        # Data[1]
-        {'point' : 'G', 'X' : 3, 'Y' : 2, 'label' : 'Red'}       # Data[2]      # Data[3]
+        {'point' : 'E', 'X' : 6, 'Y' : 6, 'label' : 'Blue'},        
+        {'point' : 'F', 'X' : 3, 'Y' : 4, 'label' : 'Red'},        
+        {'point' : 'G', 'X' : 3, 'Y' : 2, 'label' : 'Red'}            
     ]
 
     print(border)
-    print("Marvellous KNN Classifier")
+    print("KNN Classifier")
     print(border)
 
     for i in Data:
@@ -35,7 +35,7 @@ def MarvellousKNNClassifier(k = 3):
     print(border)
 
     for d in Data: 
-        d['distance'] = MarvellousEucDistance(d,new_point)
+        d['distance'] = EucDistance(d,new_point)
 
     for d in Data:
         print(d)
@@ -89,7 +89,7 @@ def MarvellousKNNClassifier(k = 3):
     print("Final prediction is: ",Name)
 
 def main():
-    MarvellousKNNClassifier()
+    KNNClassifier()
 
 if __name__ == "__main__":
     main()
