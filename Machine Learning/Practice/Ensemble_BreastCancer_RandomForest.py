d801ee64b5a9585fd30edcfa84b5ca34932a63be
ruchakhabale <rucharkhabale@gmail.com>
2026-09-03 23:13:20 +0530

    Add files via upload

---
+++ Machine Learning/Practice/Ensemble_BreastCancer_RandomForest.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# -------------------------------------------------------------------
+# Step 1 : Load the dataset
+# -------------------------------------------------------------------
+
+df = pd.read_csv("breast_cancer.csv")
+
+print("Shape of dataset : ",df.shape)
+
+print("First few records : ")
+print(df.head())
+
+# -------------------------------------------------------------------
+# Step 2 : Separate features and labels 
+# -------------------------------------------------------------------
+
+X = df.drop("target", axis=1)
+Y = df["target"]
+
+print("X shape : ",X.shape)
+print("Y shape : ",Y.shape)
+
+# -------------------------------------------------------------------
+# Step 3 : Split the dataset for training and testing
+# -------------------------------------------------------------------
+
+X_train,X_test, Y_train,Y_test = train_test_split(
+                                                X,
+                                                Y,
+                                                test_size=0.2,
+                                                random_state=42
+                                                )
+
+# -------------------------------------------------------------------
+# Step 4 : Scale the features
+# -------------------------------------------------------------------
+
+scalar = StandardScaler()
+
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.fit_transform(X_test)
+
+# -------------------------------------------------------------------
+# Step 5 : Create the Model
+# -------------------------------------------------------------------
+
+model = RandomForestClassifier(
+    n_estimators=10,                           # this creates 10 diff Decision trees
+    random_state=42
+    )
+
+# -------------------------------------------------------------------
+# Step 6 : Train the model
+# -------------------------------------------------------------------
+
+model = model.fit(X_train,Y_train)          
+
+# -------------------------------------------------------------------
+# Step 7 : Test the model
+# -------------------------------------------------------------------
+
+Y_pred = model.predict(X_test)
+
+# -------------------------------------------------------------------
+# Step 8 : Evaluate the model
+# -------------------------------------------------------------------
+
+print("Accuracy : ",accuracy_score(Y_test,Y_pred))
+
+print("Confusion Matrix : ")
+print(confusion_matrix(Y_test,Y_pred))
