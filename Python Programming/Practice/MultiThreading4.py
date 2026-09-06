205824d3fda964da63609a896bbb7783b5a5dece
ruchakhabale <rucharkhabale@gmail.com>
2026-09-06 23:41:16 +0530

    Update MultiThreading4.py

--- Python Programming/Practice/MultiThreading4.py
+++ Python Programming/Practice/MultiThreading4.py
@@ -1,5 +1,4 @@
-import threading             # threading is a name of in built module
-
+import threading             
 def Display(No1, No2, No3):      
     print(f"Inside Display {No1}, {No2}, {No3} : ",threading.get_ident())    
 
@@ -7,12 +6,10 @@ def main():
     print("Inside main : ",threading.get_ident())    
 
     #passing parameters to thread function so we need to give iterable so (11,) says tuple
-    tobj = threading.Thread(target=Display, args = (11,21,51,))      # threading naav cha module madhun Thread naav cha class cha obj banava tobj asa
+    tobj = threading.Thread(target=Display, args = (11,21,51,))      
 
-    tobj.start()   # thread chya obj cha naav.start mhnje thread chalu hoto
+    tobj.start()  
 
 
 if __name__ == "__main__":
     main()
-
-# refer DatatypeX.py for meaning of args = (11,)
