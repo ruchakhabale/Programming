80e8cb0c1e280f88fdf39fe60d140644ec59046b
ruchakhabale <rucharkhabale@gmail.com>
2026-09-06 23:39:38 +0530

    Update MultiThreading1.py

--- Python Programming/Practice/MultiThreading1.py
+++ Python Programming/Practice/MultiThreading1.py
@@ -1,10 +1,10 @@
-import threading             # threading is a name of in built module
+import threading             
 
 def Display():
-    print("Inside Display : ",threading.get_ident())   #this also gives Thread ID of main thread, as there is No other thread here, only one thread is there
+    print("Inside Display : ",threading.get_ident())   
 
 def main():
-    print("Inside main : ",threading.get_ident())      #gives Thread ID of main thread
+    print("Inside main : ",threading.get_ident())      
     Display()
 
 if __name__ == "__main__":
