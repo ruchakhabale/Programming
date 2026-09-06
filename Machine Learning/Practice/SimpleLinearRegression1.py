3e02dc3f2b4b830df5b8dd9808129f0e7f558e60
ruchakhabale <rucharkhabale@gmail.com>
2026-09-06 23:37:29 +0530

    Add files via upload

---
+++ Machine Learning/Practice/SimpleLinearRegression1.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# chk
+def main():
+    X = np.array([1, 2, 3, 4, 5])
+
+    Y = np.array([50, 55, 60, 65, 70])
+
+    model = LinearRegression()
+
+    model = model.fit(X,Y)
+
+    print(model.predict([[6]]))
+
+if __name__ == "__main__":
+    main()
