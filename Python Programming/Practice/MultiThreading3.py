b698f9f5791e35913e4d2f188278cb01a6cdf22c
ruchakhabale <rucharkhabale@gmail.com>
2026-09-06 23:40:41 +0530

    Update MultiThreading3.py

--- Python Programming/Practice/MultiThreading3.py
+++ Python Programming/Practice/MultiThreading3.py
@@ -1,4 +1,4 @@
-import threading             # threading is a name of in built module
+import threading            
 
 def Display(No):
     print(f"Inside Display {No} : ",threading.get_ident())    
@@ -6,12 +6,12 @@ def Display(No):
 def main():
     print("Inside main : ",threading.get_ident())    
 
-    tobj = threading.Thread(target=Display, args = (11,))      # threading naav cha module madhun Thread naav cha class cha obj banava tobj asa
+    tobj = threading.Thread(target=Display, args = (11,))     
 
-    tobj.start()   # thread chya obj cha naav.start mhnje thread chalu hoto
+    tobj.start()   
 
 
 if __name__ == "__main__":
     main()
 
-# refer DatatypeX.py for meaning of args = (11,)
+
