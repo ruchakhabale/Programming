c4b4aad19c27c0231e848927031faecd954226b9
ruchakhabale <rucharkhabale@gmail.com>
2026-09-07 23:44:55 +0530

    Update program268.java

--- Java_Programming/Practice/program268.java
+++ Java_Programming/Practice/program268.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class StringX
 {
-    public void Display(String str)  // haa StringX cha str aahe can use the same name as str from main as they are into diff classes
+    public void Display(String str)  s
     {
         System.out.println("Received string is : "+str);
         
@@ -14,9 +14,9 @@ class program268
 {
     public static void main(String A[])
     {
-        Scanner sobj = new Scanner(System.in);           //Scanner class object
-        String str = null;    //haa main cha str aahe    //reference
-        program280 strobj = new program280();                  //object of StringX class
+        Scanner sobj = new Scanner(System.in);                //Scanner class object
+        String str = null;                                    //reference
+        program280 strobj = new program280();                 //object of StringX class
 
         System.out.println("Enter String : ");
         str = sobj.nextLine();
