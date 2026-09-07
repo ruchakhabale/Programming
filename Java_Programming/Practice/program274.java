902fe54c3e714906c748b1af96dfecb61b3a63a5
ruchakhabale <rucharkhabale@gmail.com>
2026-09-07 23:46:59 +0530

    Update program274.java

--- Java_Programming/Practice/program274.java
+++ Java_Programming/Practice/program274.java
@@ -78,9 +78,9 @@ class program274
 {
     public static void main(String A[])
     {
-        Scanner sobj = new Scanner(System.in);     //Scanner class object
-        String data = null;                         //reference
-        StringX strobj = new StringX();            //object of StringX class
+        Scanner sobj = new Scanner(System.in);     
+        String data = null;                         
+        StringX strobj = new StringX();            
         int iRet = 0;
 
         System.out.println("Enter String : ");
