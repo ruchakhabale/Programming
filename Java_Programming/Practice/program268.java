e056944e6cc78fc7ab27ba276dfccf79b8fb8835
ruchakhabale <rucharkhabale@gmail.com>
2026-09-07 23:50:06 +0530

    Update program268.java

--- Java_Programming/Practice/program268.java
+++ Java_Programming/Practice/program268.java
@@ -14,9 +14,9 @@ class program268
 {
     public static void main(String A[])
     {
-        Scanner sobj = new Scanner(System.in);                //Scanner class object
-        String str = null;                                    //reference
-        program280 strobj = new program280();                 //object of StringX class
+        Scanner sobj = new Scanner(System.in);                
+        String str = null;                                    
+        program280 strobj = new program280();                 
 
         System.out.println("Enter String : ");
         str = sobj.nextLine();
