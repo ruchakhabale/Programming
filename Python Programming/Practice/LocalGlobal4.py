15dccf089677679fcf46fd1d2a8f9fa6a5e99fbd
ruchakhabale <rucharkhabale@gmail.com>
2026-09-08 23:49:20 +0530

    Update LocalGlobal4.py

--- Python Programming/Practice/LocalGlobal4.py
+++ Python Programming/Practice/LocalGlobal4.py
@@ -1,14 +1,10 @@
-no = 11    #global variable
+no = 11   
 
 def Display():
-    global no    # this tells i dont want to create a new no, use the global wala no 
-                #[here at this step, writing global keyword explicitly is lly to extern keyword of storage classes (PPA) ]
+    global no    
     no = 21                               
     print("From display : ",no)
 
 print("Before : ",no)
 Display()
 print("After : ",no)
-
-#open-to-all session , movie trailer - declaration
-#private sessions , actual movie - definition
