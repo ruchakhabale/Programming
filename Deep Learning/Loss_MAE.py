70164e666a784ac66ef5fe7f3159583737f6685f
ruchakhabale <rucharkhabale@gmail.com>
2026-09-08 23:41:25 +0530

    Add files via upload

---
+++ Deep Learning/Loss_MAE.py
@@ -0,0 +1,21 @@
+def MAE(Y_True, Y_Pred):
+    n = len(Y_Pred)
+    total_error = 0
+
+    for i in range(n):
+        error = abs (Y_True[i] - Y_Pred[i])
+        total_error = total_error + error
+
+    MAE = total_error / n
+
+    return MAE
+
+Y_True = [10,20,30]
+Y_Pred = [12,18,33]
+
+loss = MAE(Y_True,Y_Pred)
+
+print("Loss is : ",loss)
+
+
+# why we don't take sqaure for MAE and why sq for MSE, write explanation in nb (SN)
