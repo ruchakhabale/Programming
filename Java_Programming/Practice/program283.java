f8c3446d32f6c3d85da3ab398668bc4c57f0f873
ruchakhabale <rucharkhabale@gmail.com>
2026-09-08 23:43:22 +0530

    Update program283.java

--- Java_Programming/Practice/program283.java
+++ Java_Programming/Practice/program283.java
@@ -5,6 +5,6 @@ class program283
             char ch1 = 'A';
 
             System.out.println(ch1);
-            System.out.println((char)(ch1 +32));   // purna aalela ans la typecast karun char madhe convert kela this will be character + character
+            System.out.println((char)(ch1 +32));   
         }   
 }
