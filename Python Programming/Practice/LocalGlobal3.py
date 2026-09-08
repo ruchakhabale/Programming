835e26bb2e0b34bc42bd62e6c5d039ba1d797080
ruchakhabale <rucharkhabale@gmail.com>
2026-09-08 23:48:50 +0530

    Update LocalGlobal3.py

--- Python Programming/Practice/LocalGlobal3.py
+++ Python Programming/Practice/LocalGlobal3.py
@@ -1,13 +1,9 @@
-no = 11    #global variable
+no = 11    
 
 def Display():
-    no = 21          # this line creates a new no which stores 21 value NOT the global value of 11                              
+    no = 21                                      
     print("From display : ",no)
 
 print("Before : ",no)
 Display()
 print("After : ",no)
-
-
-# complier finds out only the syntactical errors only 
-# for logical errors the execution stops at intepretation level
