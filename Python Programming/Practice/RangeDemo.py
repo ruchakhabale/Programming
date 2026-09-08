1bc2404f8555d3331e6e905b0cb88725047497fa
ruchakhabale <rucharkhabale@gmail.com>
2026-09-08 23:50:35 +0530

    Update RangeDemo.py

--- Python Programming/Practice/RangeDemo.py
+++ Python Programming/Practice/RangeDemo.py
@@ -1,7 +1,7 @@
 Data = range(5)
 
 print(Data)
-print(type(Data))   #this step says that range is a Datatype
+print(type(Data))  
 
 Data1 = range(1,7)
 print(Data1)
@@ -9,6 +9,6 @@ print(Data1)
 Data2 = range(3,9,2)
 print(Data2)
 
-print(len(Data))   #this step says that range is a Datatype as well as a sequence too
+print(len(Data))   
 print(len(Data1)) 
 print(len(Data2)) 
