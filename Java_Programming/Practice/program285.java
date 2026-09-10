cfdd15600e4b6c91cc76ff98fb076e618e74be25
ruchakhabale <rucharkhabale@gmail.com>
2026-09-10 23:47:43 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program285.java
@@ -0,0 +1,39 @@
+// accept input from user and update to uppercase
+
+import java.util.*;
+
+class StringX
+{
+    public String toUpperX(String str)
+    {
+        int i = 0;
+
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i<Arr.length; i++)
+        {
+            Arr[i] = (char) (Arr[i] - 32);    // ISSUE
+        }
+        return new String(Arr); //he directly new string banavun return karta, refere 278 for reference, preferably use this 
+    }
+}
+
+class program285
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);     
+        String data = null;                         
+        StringX strobj = new StringX();   
+        String sRet = null;        
+        
+        System.out.println("Enter String : ");
+        data = sobj.nextLine();
+
+        sRet = strobj.toUpperX(data);
+
+        System.out.println("Updated string is : "+sRet);
+
+    }
+}
+
