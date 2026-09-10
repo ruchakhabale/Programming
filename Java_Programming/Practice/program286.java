56ef6f65c242848a3d0d0ed70354b8efd7b60d29
ruchakhabale <rucharkhabale@gmail.com>
2026-09-10 23:49:23 +0530

    Update program286.java

--- Java_Programming/Practice/program286.java
+++ Java_Programming/Practice/program286.java
@@ -16,7 +16,7 @@ class StringX
                 Arr[i] = (char)(Arr[i] - 32);   
             }
         }
-        return new String(Arr); //he directly new string banavun return karta, refere 278 for reference, preferably use this 
+        return new String(Arr); 
     }
 }
 
