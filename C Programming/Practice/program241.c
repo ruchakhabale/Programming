7deae82f64666e13e8b44fa332e31a50e6dfc66d
ruchakhabale <rucharkhabale@gmail.com>
2026-09-13 23:52:25 +0530

    Update program241.c

--- C Programming/Practice/program241.c
+++ C Programming/Practice/program241.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 
-
 int main()
 {
-    char Arr[50] = {'\0'};   //empty array set to its default value of character
+    char Arr[50] = {'\0'};   
 
     printf("Enter String : \n");
-    scanf("%[^'\n]s",Arr);  // [^'\n]s this is regex(regular expression)  this line is used to tell that acceptthe ip untill you get the new line character i.e. when we press enter 
+    scanf("%[^'\n]s",Arr);  // regular expression
 
     printf("Entered String is : %s\n",Arr);
-
 
     return 0;
 }
