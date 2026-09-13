608f243979f780ae81b428199ab637fa077d1048
ruchakhabale <rucharkhabale@gmail.com>
2026-09-13 23:48:55 +0530

    Update program240.c

--- C Programming/Practice/program240.c
+++ C Programming/Practice/program240.c
@@ -4,10 +4,10 @@
 
 int main()
 {
-    char Arr[50] = {'\0'};   //empty array set to its default value of character
+    char Arr[50] = {'\0'};  
 
     printf("Enter String : \n");
-    scanf("%s",Arr);   // Issues (accepts data only till we dont use the first space, like jay ganesh, i.e. doesnt accept the whitespace)  here, we dont need, &Arr bcoz here Arr is an Array, so nusta Arr gives base address only
+    scanf("%s",Arr);   // Issue
 
     printf("Entered String is : %s\n",Arr);
 
