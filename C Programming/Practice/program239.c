cd5839dd1db28528b51c81ea5ff24f6bbbc7fac4
ruchakhabale <rucharkhabale@gmail.com>
2026-09-13 23:48:17 +0530

    Update program239.c

--- C Programming/Practice/program239.c
+++ C Programming/Practice/program239.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 
-
 int main()
 {
     char * str = "Ganesh";
     int iCount = 0;
 
-    printf("length of string is : %lu\n",strlen(str));   //if this line is after the loop, it will stop at \0 so its gives 0 as the op
+    printf("length of string is : %lu\n",strlen(str));   
 
     while(*str != '\0')
     {
@@ -17,7 +16,6 @@ int main()
 
     printf("Length of string is : %d\n",iCount);
  
-
     return 0;
 }
 
