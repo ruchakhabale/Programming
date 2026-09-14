5dd845bd6a98ad6de816003f944d66223d0db268
ruchakhabale <rucharkhabale@gmail.com>
2026-09-14 23:52:14 +0530

    Update program264.java

--- Java_Programming/Practice/program264.java
+++ Java_Programming/Practice/program264.java
@@ -5,7 +5,7 @@ class program264
     public static void main(String A[])
     {
         Scanner sobj = new Scanner(System.in);
-        String Arr = null;  //this Arr is reference, it is not an Array
+        String Arr = null;  
 
 
         System.out.println("Enter string : ");
