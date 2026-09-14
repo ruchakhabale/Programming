3d96299cd084d3442f2d23219d8059439528d03c
ruchakhabale <rucharkhabale@gmail.com>
2026-09-14 23:51:08 +0530

    Update program261.java

--- Java_Programming/Practice/program261.java
+++ Java_Programming/Practice/program261.java
@@ -5,13 +5,13 @@ class program261
     public static void main(String A[])
     {
         Scanner sobj = new Scanner(System.in);
-        String Arr = null;  //this Arr is reference, it is not an Array
+        String Arr = null;  
 
 
         System.out.println("Enter string : ");
         Arr = sobj.nextLine();   
  
-        // Error as it doesnt get treated as an array so cant be accessed using index, in c it used to get treated as character array 
+        // Error as it doesnt get treated as an array 
         System.out.println(Arr[0]);
         System.out.println(Arr[1]);
         System.out.println(Arr[2]);
