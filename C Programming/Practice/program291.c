b69bc776d4e5494b4c33bbc9d7b9513d1a6444fb
ruchakhabale <rucharkhabale@gmail.com>
2026-09-16 23:16:28 +0530

    Update program291.c

--- C Programming/Practice/program291.c
+++ C Programming/Practice/program291.c
@@ -1,21 +1,20 @@
-// reverse display karna ani reverse karna are 2 diff things 
 #include<stdio.h>
 
 void ReverseDisplay(char * str)
 {
     char *start = NULL;
 
-    start = str;    //start naav cha 2nd pointer holds the address of str i.e. 100
+    start = str;    
 
     while(*str != '\0')
     {
         str++;
     }
-    // Issue (the gap seen in the op, refer op for that )
+    
     while(start <= str)
     {
-        printf("%c\n",*str);   //*str mhnje ek ek single letter 
-        str--;    //loop ulta fhirto 
+        printf("%c\n",*str);   
+        str--;    
     }  
 }
 
