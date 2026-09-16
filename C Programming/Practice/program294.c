abd106fb42ed970afa32d9c10f9dc3a81457708b
ruchakhabale <rucharkhabale@gmail.com>
2026-09-16 23:18:15 +0530

    Update program294.c

--- C Programming/Practice/program294.c
+++ C Programming/Practice/program294.c
@@ -1,4 +1,3 @@
-// reverse display karna ani reverse karna are 2 diff things 
 #include<stdio.h>
 
 void ReverseDisplay(char * str)
@@ -16,7 +15,7 @@ void ReverseDisplay(char * str)
 
     while(start <= str)
     {
-        printf("%s\n",str);  // %s will display the data till it gets '\0 ; it needs address, so *str nahi fakt str dila aahe 
+        printf("%s\n",str);   
         str--;    
     } 
     printf("\n"); 
@@ -33,8 +32,3 @@ int main()
 
     return 0;
 }
-
-/*
-in context of op of this program
-there is no need to draw matrix for this program 
-*/
