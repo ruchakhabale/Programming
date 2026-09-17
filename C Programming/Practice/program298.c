e4f12357a78ec1ac21a917ca37411be992f4f913
ruchakhabale <rucharkhabale@gmail.com>
2026-09-17 23:57:00 +0530

    Update program298.c

--- C Programming/Practice/program298.c
+++ C Programming/Practice/program298.c
@@ -1,27 +1,23 @@
-// swapping R.L.ex. of hot choco, nutella and the temp mug i.e. using 3 variables 
 #include<stdio.h>
 
-// Call by Address
 void Swap(int *ptr1, int *ptr2)
 {
     int temp = 0;
 
-    //ethe he direct change zale
     temp = *ptr1;
     *ptr1 = *ptr2;
     *ptr2 = temp;
 
 }
 
-
 int main()
 {
     int i = 11;
     int j = 21;
 
-    int temp = 0;  // 3rd cup 
+    int temp = 0; 
 
-    Swap(&i,&j);   // Call by Address
+    Swap(&i,&j);   
 
     printf("%d\n",i);
     printf("%d\n",j);
