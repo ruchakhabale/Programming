0e354f02ce9b706d3dcf9c406f016c0516706be0
ruchakhabale <rucharkhabale@gmail.com>
2026-09-21 23:45:02 +0530

    Update program166.cpp

--- C++_Programming/Practice/program166.cpp
+++ C++_Programming/Practice/program166.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-//chk, related to spring boot, refer again after learning 
+
 #pragma pack(1)
 class ArrayX
 {
@@ -11,7 +11,7 @@ class ArrayX
     public:
         ArrayX()
         {
-            iSize = 5;         //default value deli aahe ethe 5 ashi, it can be anything as per need             
+            iSize = 5;                     
             Arr = new int [iSize];      
         }
 
