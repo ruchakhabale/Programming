0a6ccb96abbcd4418e59b1cc39493adbff711db6
ruchakhabale <rucharkhabale@gmail.com>
2026-09-24 23:07:49 +0530

    Update CommandLine2.py

--- Python Programming/Practice/CommandLine2.py
+++ Python Programming/Practice/CommandLine2.py
@@ -1,6 +1,6 @@
-import sys     # sys for system
+import sys  
 
-print(sys.argv[0])    #filename display karnar refer diagram from nb where python Addition.py 10 11 enter wle line
+print(sys.argv[0])    
 
 print(sys.argv[1])
 
