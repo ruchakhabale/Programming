f696d2b168ccde6515abe05a2f635e385a8da762
ruchakhabale <rucharkhabale@gmail.com>
2026-09-24 23:09:49 +0530

    Update Default.py

--- Python Programming/Practice/Default.py
+++ Python Programming/Practice/Default.py
@@ -1,13 +1,13 @@
-def Area(Radius, PI = 3.14):    # jar tu dusari point nahi pathvali tar mii by default 3.14 gheto 
+def Area(Radius, PI = 3.14):     
     Ans = PI * Radius * Radius
     return Ans
 
 
 def main():
-    Ret = Area(10.5)     #ethe tyanchi default use keli 3.14 karan 2nd parameter is not given 
+    Ret = Area(10.5)    
     print("Area of circle is : ",Ret)
 
-    Ret = Area(10.5,7.12)     #ethe apan 7.12 dila aahe tee default chy 3.14 la khodun hee 7.12 use keli jaate
+    Ret = Area(10.5,7.12)    
     print("Area of circle is : ",Ret)
 
 
