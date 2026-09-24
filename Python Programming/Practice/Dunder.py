76918ab57fcb04a313da21ed5aa3562e001523d9
ruchakhabale <rucharkhabale@gmail.com>
2026-09-24 23:10:40 +0530

    Update Dunder.py

--- Python Programming/Practice/Dunder.py
+++ Python Programming/Practice/Dunder.py
@@ -1,6 +1,4 @@
-#Dunder - Double underscore
-
 
 print("Inside Python Program")
 
-print(__name__)    # __main__ mazha naav main aahe, mhnje mii main aahe (file la vicharycha tuzha naav kyy) 
+print(__name__)    
