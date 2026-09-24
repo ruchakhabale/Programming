3cc98a0c239b47bb51bce1f2d265e39127ca3377
ruchakhabale <rucharkhabale@gmail.com>
2026-09-24 23:06:20 +0530

    Update AbstractDemo2.py

--- Python Programming/Practice/AbstractDemo2.py
+++ Python Programming/Practice/AbstractDemo2.py
@@ -15,9 +15,3 @@ dobj = Derived()
 Ret = dobj.Addition(10,11)
 
 print("Addition is : ",Ret)
-
-
-# full form : abstract base class
-# abc module name
-# ABC class name, for writing an Abtract method, we need to inherit this class 
-# abstractmethod naava cha decorator pnn magvla wala (imoprt wali stamt madhe )
