0909da7fc6f98d491db198169b659e559f2af56d
ruchakhabale <rucharkhabale@gmail.com>
2026-09-24 23:07:06 +0530

    Update ClassDemo1.py

--- Python Programming/Practice/ClassDemo1.py
+++ Python Programming/Practice/ClassDemo1.py
@@ -5,8 +5,3 @@ class Demo:
 #object creation of Demo class
 obj1 = Demo()    
 obj2 = Demo() 
-
-
-# __init__  haa python cha constructor aahe, this name is fixed for it 
-
-# toh (self)   haa this pointer of Python aahe 
